bfad63f00168a42b15e55f31bf1de3e0a548e6be
ammasvilasini <AMMASvilasini@MacBook-Pro-2.local>
2016-05-22 01:58:49 +0530

    re-adding big break marina

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,10 @@
    
  
+    $('.thumbnail').hover(
+        function(){
+            $(this).find('.caption').slideDown(250); //.fadeIn(250)
+        },
+        function(){
+            $(this).find('.caption').slideUp(250); //.fadeOut(205)
+        }
+    ); 
